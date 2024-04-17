193f73ea4e73e9dcc6b76e2ceb75287cedde98f0
Andika Dwiki Darmawan <andika0106dwiki@gmail.com>
2024-04-17 11:58:12 +0700

    bagaimana jika kita tambah bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }} | Prefix</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     {{ $styles }}
 </head>
 
@@ -16,8 +16,10 @@
     ny ke file .blade nya --}}
     <x-navbar></x-navbar>
     {{-- End --}}
-    {{ $slot }}
-    {{-- <script src="{{ asset('js/app.js') }}"></script> --}}
+    <div class="pt-4">
+        {{ $slot }}
+    </div>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
 </body>
 
 </html>
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,25 +1,38 @@
 <x-app-layout title="Tasks">
-    <h1>Tasks</h1>
 
-    <form action="/tasks" method="post">
-        @csrf
-        <input type="text" name="list" id="" placeholder="The name of task">
-        <button type="submit">Add</button>
-    </form>
-
-    <ul style="list-style-type: none">
-        @foreach ($tasks as $index => $task)
-            <li>{{ $index + 1 }} - {{ $task->list }}
-                <div style="display: inline; margin-left: 10px">
-                    <a style="color:skyblue; margin-right: 10px" href="/tasks/{{ $task->id }}/edit">Edit</a>
-                    <form action="/tasks/{{ $task->id }}" method="post" style="display: inline">
-                        @method('delete')
-                        @csrf
-                        <button type="submit">Delete</button>
-                    </form>
+    <div class="container">
+        <h1>Tasks</h1>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">Create New Task</div>
+                    <div class="card-body">
+                        <form action="/tasks" method="post" class="d-flex">
+                            @csrf
+                            <input type="text" class="form-control me-2" name="list" id=""
+                                placeholder="The name of task">
+                            <button class="btn btn-primary" type="submit">Add</button>
+                        </form>
+                    </div>
                 </div>
-            </li>
             </div>
-        @endforeach
-    </ul>
+        </div>
+
+        <ul class="list-group mt-4">
+            @foreach ($tasks as $index => $task)
+                <li class="list-group-item d-flex align-items-center justify-content-between">{{ $index + 1 }} -
+                    {{ $task->list }}
+                    <div class="d-flex gap-2">
+                        <a class="btn btn-primary" href="/tasks/{{ $task->id }}/edit">Edit</a>
+                        <form action="/tasks/{{ $task->id }}" method="post" style="display: inline">
+                            @method('delete')
+                            @csrf
+                            <button class="btn btn-danger" type="submit">Delete</button>
+                        </form>
+                    </div>
+                </li>
+            @endforeach
+        </ul>
+    </div>
+
 </x-app-layout>
